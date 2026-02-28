f7778f4328fe61d181fa55b840de8ea1cceba1e7
divya07-oh <divyavenkatesan239@gmail.com>
2026-02-28 20:18:48 +0530

    updating file

--- backend/controllers/queueController.js
+++ backend/controllers/queueController.js
@@ -1,17 +1,28 @@
-let queue = [];
-let currentToken = 0;
-exports.joinQueue = (req,res) =>{
+let queue = []; //empty list
+let currentToken = 0; //start 0
+exports.joinQueue = (req,res) =>{ //client request anupuna ishu function run agum
     currentToken++;
     const newEntry = {
         token: currentToken,
-        status: "waiting"
+        status: "waiting" //pudhu person create panrom
     };
     queue.push(newEntry);
     res.json({
-        message:"joined queue successfully",
+        message:"joined queue successfully", // response anuprom
         token: currentToken
     });
 };
 exports.getQueue = (req,res) =>{
     res.json(queue);
+};
+exports.serveNext = (req,res) => {
+    const nextPerson = queue.find(person => person.status === "waiting");
+    if(!nextPerson){
+        return res.json({ message: "No one in waiting"});
+    }
+    nextPerson.status = "served";
+    res.json({
+        message: "Next token served",
+        token: nextPerson.token
+    });
 };
--- backend/routes/queueRoutes.js
+++ backend/routes/queueRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const queueController = require("../controllers/queueController.js");
 router.post("/join", queueController.joinQueue);
 router.get("/all", queueController.getQueue);
+router.put("/serve", queueController.serveNext);
 module.exports = router;
